a06cd08f776a64a901d867c6294a5cae587bee97
Karim Zamrini <karim.e.zamrini@gmail.com>
2021-02-25 11:38:41 -0700

    Completed Module/Component File Skeleton

---
+++ client/app/components/QuestionsAndAnswers/AddQuestionForm/AddQuestion.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default
+
+mkdir QuestionsAndAnswers; cd QuestionsAndAnswers; touch QuestionsAndAnswers.jsx; touch QuestionsAndAnswers.css; cd ..
--- client/app/components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
+++ client/app/components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -1,3 +1,6 @@
 import React from 'react';
 
 export default
+
+
+// More answered questions button
