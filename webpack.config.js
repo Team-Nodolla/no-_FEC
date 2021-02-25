68dc189bbffbd7eecad5a350f587b998f256c0e9
Karim Zamrini <karim.e.zamrini@gmail.com>
2021-02-24 17:00:45 -0700

    linting cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -12,15 +12,15 @@ module.exports = {
       {
         test: /\.jsx?$/,
         // exclude: [
-          // 'node_modules'
+        // 'node_modules'
         // ],
         use: [{
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-react', '@babel/preset-env']
-          }
-        }]
-      }
-    ]
-  }
+          },
+        }],
+      },
+    ],
+  },
 };
