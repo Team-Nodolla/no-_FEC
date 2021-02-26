8087fa5d638d4145afebba02361ba8035222e12e
Khoi Dao <khoidao55@gmail.com>
2021-02-26 15:51:07 -0800

    added a random boolean test to jest in order to see if circleCI clears.

--- jestTest.test.js
+++ jestTest.test.js
@@ -4,4 +4,9 @@ const sumTest = () => {
   expect(sum(1, 2)).toBe(3);
 };
 
+const boolTest = () => {
+  expect(true).toBe(true);
+};
+
 test('adds 1 + 2 to equal 3', sumTest);
+test('random booleanTest', boolTest);
