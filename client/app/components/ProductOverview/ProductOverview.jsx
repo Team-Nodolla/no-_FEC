d1a6ed6adc830fcdc72035bf4532c876b9c969bd
Khoi Dao <khoidao55@gmail.com>
2021-03-01 10:05:07 -0800

    changed a few things to product overview

--- client/app/components/ProductOverview/ProductOverview.jsx
+++ client/app/components/ProductOverview/ProductOverview.jsx
@@ -3,15 +3,18 @@ import axios from 'axios';
 import ProductCategoryAndTitle from './ProductCategoryAndTitle/ProductCategoryAndTitle.jsx';
 
 const ProductOverview = ({ productID, product }) => {
-  //const [styles, getStyles] = useState('');
-    // getStyles(() => axios.get(`/products/:${product}/styles`)
-    //   .then((data) => console.log(data)))
-    //   .catch((err) => console.log('error'));
-    return (
-      <div>
-        <ProductCategoryAndTitle product={product} />
-      </div>
-    );
+  const [styles, getStyles] = useState('');
+  getStyles(() => axios.get(`/products/${productID}`)
+    .then((data) => console.log(data))
+    .catch((err) => console.log('error')));
+
+  console.log('hello');
+  console.log(productID);
+  return (
+    <div>
+      <ProductCategoryAndTitle product={product} />
+    </div>
+  );
 };
 
 export default ProductOverview;
