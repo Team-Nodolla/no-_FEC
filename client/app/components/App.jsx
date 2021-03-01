a6e195bc5a9a8c4d8ac1fc3de11522f7e44828ab
tdagai <trew3535@gmail.com>
2021-03-01 12:24:11 -0800

    Created Carousel Card component, added proptypes to package.json, changed the name of StarReview -> StarRating to match file name

--- client/app/components/App.jsx
+++ client/app/components/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/extensions */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ProductOverview from './ProductOverview/ProductOverview.jsx';
