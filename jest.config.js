ef3498c6aec7c57284bb2ae3cbfd46a170d46d61
Karim Zamrini <karim.e.zamrini@gmail.com>
2021-03-02 11:37:01 -0700

    updated testing tools and compatibility

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,11 @@ module.exports = {
 
   moduleFileExtensions: ['js', 'json', 'jsx'],
 
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.css$': 'identity-obj-proxy',
+  },
+
   setupFiles: ['<rootDir>/enzyme.config.js'],
 
   testEnvironment: 'jsdom',
