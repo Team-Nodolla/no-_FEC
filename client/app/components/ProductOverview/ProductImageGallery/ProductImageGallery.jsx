80d019558530be262e79934f7908915ec08848e2
tdagai <trew3535@gmail.com>
2021-03-03 11:49:31 -0800

    merging

--- client/app/components/ProductOverview/ProductImageGallery/ProductImageGallery.jsx
+++ client/app/components/ProductOverview/ProductImageGallery/ProductImageGallery.jsx
@@ -2,34 +2,34 @@ import React, { useState } from 'react';
 import propTypes from 'proptypes';
 import DefaultView from './DefaultView/DefaultView.jsx';
 import ExpandedView from './ExpandedView/ExpandedView.jsx';
+import ProductThumbnailScroll from './ProductThumbnailScroll/ProductThumbnailScroll.jsx';
 import './ProductImageGallery.css';
 
 const ProductImageGallery = ({ style }) => {
-  if (style !== '') {
+  if (Object.keys(style).length !== 0) {
     const toggle = false;
     const [currentPhoto, setCurrentPhoto] = useState(0);
     const [expandView, setExpandView] = useState(toggle);
 
     const onClickRightChange = () => {
-      console.log('clicked right');
       if (currentPhoto !== style.photos.length - 1) {
         setCurrentPhoto(currentPhoto + 1);
       }
     };
 
     const onClickLeftChange = () => {
-      console.log('clicked left');
       if (currentPhoto > 0) {
         setCurrentPhoto(currentPhoto - 1);
       }
     };
+
     const onClickZoom = () => {
-      console.log('clicked on image', toggle);
       setExpandView((view) => !view);
     };
 
     return (
       <div>
+        <div className="product-thumbnail"><ProductThumbnailScroll currentPhoto={style.photos[currentPhoto].thumbnail_url} arrayOfPhoto={style} /></div>
         {
         expandView === false
           ? (
@@ -57,7 +57,7 @@ const ProductImageGallery = ({ style }) => {
             </div>
           )
           : (
-            <div className="modal">
+            <div onClickImage={onClickZoom} className="modal">
               <ExpandedView
                 onClickImage={onClickZoom}
                 defaultPhoto={style.photos[currentPhoto]}
--- client/app/components/helperFunctions/getAverageScore.jsx
+++ client/app/components/helperFunctions/getAverageScore.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 const getAverageScore = (ratings) => {
   // gathers totalScore
   let totalScore = 0;
+  let numberOfRatings = 0;
   // create tupple for each rating 1-5
   const ratingsArray = Object.entries(ratings);
   // if there are no ratings, return null
@@ -13,9 +14,10 @@ const getAverageScore = (ratings) => {
   // for each rating, add it to totalScore
   for (let i = 0; i < ratingsArray.length; i += 1) {
     totalScore += ratingsArray[i][0] * ratingsArray[i][1];
+    numberOfRatings += ratingsArray[i][1] * 1;
   }
   // get average score of all ratings
-  totalScore /= ratingsArray.length;
+  totalScore /= numberOfRatings;
   // round average score to nearest 10th
   totalScore = Math.round(totalScore * 10) / 10;
   // return average score
