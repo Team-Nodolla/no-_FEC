b0bfafb7927c2353f002b26bbb7a4e356167906f
Karim Zamrini <karim.e.zamrini@gmail.com>
2021-03-03 12:27:14 -0700

    fixed average rating helper

--- client/app/components/RatingsAndReviews/ReviewList/ReviewList.jsx
+++ client/app/components/RatingsAndReviews/ReviewList/ReviewList.jsx
@@ -1,3 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import propTypes from 'proptypes';
+import ReviewListItem from './ReviewListItem/ReviewListItem.jsx';
+import './ReviewList.css';
 
-export default
+const ReviewList = ({ reviewList }) => {
+  const [visibleReviews, setVisibleReviews] = useState(2);
+  // const [reviewElements, setReviewElements] = useState([]);
+  let reviewsArray = [];
+
+  reviewList.map((review) => {
+    reviewsArray.push(<ReviewListItem review={review} />);
+  });
+
+  console.log('reviews array: ', reviewsArray);
+
+  return (
+    <div className="reviewListItemContainer">
+      {reviewsArray}
+    </div>
+  );
+};
+
+ReviewList.propTypes = {
+  reviewList: propTypes.array,
+};
+
+export default ReviewList;
