a3de06f21e70cba236525b859f2dd091e94c51b6
Karim Zamrini <karim.e.zamrini@gmail.com>
2021-03-03 18:17:06 -0700

    fixed test

--- client/app/components/RatingsAndReviews/RatingsAndReviews.test.jsx
+++ client/app/components/RatingsAndReviews/RatingsAndReviews.test.jsx
@@ -2,11 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import RatingsAndReviews from './RatingsAndReviews';
 
-describe('Ratings and reviews parent component', () => {
-  test('it renders correctly', () => {
-    const wrapper = shallow(<RatingsAndReviews />);
+test('it renders correctly', () => {
+  const wrapper = shallow(<RatingsAndReviews />);
 
-    expect(wrapper).toMatchSnapshot();
-  }),
-
-})
+  expect(wrapper).toMatchSnapshot();
+});
