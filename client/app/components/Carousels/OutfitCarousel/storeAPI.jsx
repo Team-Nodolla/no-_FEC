bd41ac35e29b6955e1f3647b5a3b0083cf354ad2
tdagai <trew3535@gmail.com>
2021-03-04 01:50:00 -0800

    Added descriptions to all store methods. I wanted my first API to have really good documentation :)

--- client/app/components/Carousels/OutfitCarousel/storeAPI.jsx
+++ client/app/components/Carousels/OutfitCarousel/storeAPI.jsx
@@ -9,9 +9,15 @@ const store = {};
 /*   save   */
 /* ******** */
 
-// Input: a unique key and a value to save to the localStorage.
-// The input is stringifyed to be parsed on a get.
+// Input: A unique key and a value to save to the localStorage.
 // Output: N/A
+
+// Description:
+//              Saves a new item to localStorage with the supplied unique key
+//               as its index and the value as its value.
+//              The value is stringifyed to be parsed later by store.get().
+//              If an item with the supplied key already exists in localStorage, throws an error
+//               and doesn't save the item.
 store.save = (key, value) => {
   if (!store.has(key)) {
     localStorage.setItem(key, JSON.stringify(value));
@@ -26,7 +32,12 @@ store.save = (key, value) => {
 /* ******* */
 
 // Input: a key to search for in localStorage
-// Output: A value stored with this key as its index
+// Output: A value stored with this key as its index or null.
+
+// Description:
+//              Searchs localStorage for an item with the supplied key as its index.
+//              Returns the value of that item if it is found.
+//              Returns null if no item with this key exists in localStorage
 store.get = (key) => (
   JSON.parse(localStorage.getItem(key))
 );
@@ -36,7 +47,11 @@ store.get = (key) => (
 /* ********** */
 
 // Input: N/A
-// Output: An array containing all data currently stored in localStorage
+// Output: An array containing all data currently stored in localStorage.
+
+// Description:
+//              Retreives all items stored in localStorage as an array of objects.
+//              Returns an empty array if no items are currently stored in localStorage.
 store.getAll = () => {
   const { length, ...relevantData } = localStorage;
   const dataToReturn = [];
@@ -54,7 +69,11 @@ store.getAll = () => {
 /* ******* */
 
 // Input: a key to search for in localStorage
-// Output: true if an item with this key exists, false if not
+// Output: boolean
+
+// Description:
+//              Searches localStorage for an item with the supplied key.
+//              Returns true if it is found, otherwise returns false.
 store.has = (key) => {
   if (store.get(key)) {
     return true;
@@ -66,8 +85,13 @@ store.has = (key) => {
 /*   delete   */
 /* ********** */
 
-// Input: a key for an item to delete from localStorage
-// Output: returns the value of the deleted item
+// Input: A key for an item to delete from localStorage
+// Output: The value of the deleted item or null
+
+// Description:
+//              Deletes an item in localStorage with the supplied key as its index.
+//              Returns the value of the deleted item, or null if no item with that key
+//               exists in localStorage.
 store.delete = (key) => {
   const toRemove = store.get(key);
   localStorage.removeItem(key);
@@ -80,6 +104,9 @@ store.delete = (key) => {
 
 // Input: N/A
 // Output: N/A
+
+// Description:
+//              Deletes all data from localStorage.
 store.deleteAll = () => {
   localStorage.clear();
 };
@@ -90,6 +117,9 @@ store.deleteAll = () => {
 
 // Input: N/A
 // Output: The number of items stored in localStorage
+
+// Description:
+//              Returns the number of items stored in localStorage
 store.size = () => (
   localStorage.length
 );
@@ -100,6 +130,12 @@ store.size = () => (
 
 // Input: a key that already exists in localStorage and a new value
 // Output: N/A
+
+// Description:
+//              Updates an existing item with that has supplied key as its index
+//               with a new value.
+//              If no item with this key exists in localStorage, throws an error
+//               and doesn't update the item.
 store.update = (key, newValue) => {
   if (store.has(key)) {
     localStorage.setItem(key, JSON.stringify(newValue));
