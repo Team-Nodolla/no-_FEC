25d2d574e255cfdf4245406aadce2e0aa8c71f11
tdagai <trew3535@gmail.com>
2021-03-04 10:59:42 -0800

    Product info is now being stored in localStorage and displaying all cards saved in localStorage upon first meaningful render

--- client/app/components/Carousels/OutfitCarousel/OutfitCarousel.jsx
+++ client/app/components/Carousels/OutfitCarousel/OutfitCarousel.jsx
@@ -1,32 +1,59 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable import/extensions */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import propTypes from 'proptypes';
 import CarouselCard from '../CarouselCard/CarouselCard.jsx';
+import store from './storeAPI.jsx';
 import './OutfitCarousel.css';
 
-const OutfitCarousel = ({ productInfo }) => {
+const OutfitCarousel = ({ productInfo, handleRedirect }) => {
   const [outfitList, setOutfitList] = useState([]);
+
+  // on Mount
+  useEffect(() => {
+    if (productInfo.id !== 0) {
+      const outfitsInStore = [];
+      console.log(store.getAll());
+      store.getAll().forEach((item) => {
+        outfitsInStore.push(
+          <CarouselCard
+            key={item.id}
+            {...item}
+            handleRedirect={handleRedirect}
+            buttonFunc={console.log.bind(null, 'click')}
+            carouselType="outfit"
+          />,
+        );
+      });
+      setOutfitList(outfitsInStore);
+    }
+  }, [productInfo]);
 
   const handleAddToOutfit = () => {
     const prevSate = outfitList;
-    const newCard = (
-      <CarouselCard
-        {...productInfo}
-        buttonFunc={console.log.bind(null, 'click')}
-        carouselType="outfit"
-      />
-    );
-    if (prevSate.length === 0) {
-      setOutfitList([newCard]);
-    } else {
-      setOutfitList([...prevSate, newCard]);
+    if (productInfo.id !== 0 && !store.has(productInfo.id)) {
+      console.log(productInfo.id);
+      store.save(productInfo.id, productInfo);
+      const newCard = (
+        <CarouselCard
+          {...productInfo}
+          handleRedirect={handleRedirect}
+          buttonFunc={console.log.bind(null, 'click')}
+          carouselType="outfit"
+        />
+      );
+      if (prevSate.length === 0) {
+        setOutfitList([newCard]);
+      } else {
+        setOutfitList([...prevSate, newCard]);
+      }
     }
   };
 
   return (
     <>
       <h2 id="outfit-carousel-title">Your Outfit</h2>
+      <button type="button" onClick={() => { store.deleteAll(); }}>clear storage</button>
       <div id="outfit-carousel">
         <button type="button" id="add-to-outfit-btn" onClick={() => { handleAddToOutfit(); }}>
           <i className="fas fa-plus-square" />
@@ -42,7 +69,7 @@ const OutfitCarousel = ({ productInfo }) => {
 };
 
 OutfitCarousel.propTypes = {
-  productInfo: propTypes.any,
+  productInfo: propTypes.object,
 };
 
 export default OutfitCarousel;
