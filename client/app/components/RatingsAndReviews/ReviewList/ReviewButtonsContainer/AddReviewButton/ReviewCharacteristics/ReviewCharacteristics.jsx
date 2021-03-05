b17509916aff8188cdb201a39a1cb1319361572f
Karim Zamrini <karim.e.zamrini@gmail.com>
2021-03-05 11:21:59 -0700

    prop drilling and bug fix

--- client/app/components/RatingsAndReviews/ReviewList/ReviewButtonsContainer/AddReviewButton/ReviewCharacteristics/ReviewCharacteristics.jsx
+++ client/app/components/RatingsAndReviews/ReviewList/ReviewButtonsContainer/AddReviewButton/ReviewCharacteristics/ReviewCharacteristics.jsx
@@ -5,23 +5,17 @@ const ReviewCharacteristics = ({ metaData, reviewCharsObj, setReviewCharsObj }) 
   // for each characteristic,
   const characteristicArray = [];
 
+  // eslint-disable-next-line no-restricted-syntax
   for (const [key, value] of Object.entries(metaData.characteristics)) {
-    console.log(key, value) // sting, object
+    console.log(key, value); // sting, object
     characteristicArray.push(
-      <ReviewCharacteristic name={key} id={value.id} />
-    )
+      <ReviewCharacteristic name={key} id={value.id} />,
+    );
   }
 
   return (
     characteristicArray
-  )
-
-  // .forEach((characteristic) => {
-  //   console.log('characteristic in forEach: ', characteristic);
-  //   characteristicArray.push(
-  //     <ReviewCharacteristic characteristic={characteristic} />
-  //   );
-  // });
+  );
 };
 
 export default ReviewCharacteristics;
