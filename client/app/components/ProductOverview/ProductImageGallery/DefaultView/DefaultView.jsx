c0335c2fd68937680a1fd407d2c81a67a67aec29
Khoi Dao <khoidao55@gmail.com>
2021-03-05 12:29:37 -0800

    added thumbnail to the expanded view

--- client/app/components/ProductOverview/ProductImageGallery/DefaultView/DefaultView.jsx
+++ client/app/components/ProductOverview/ProductImageGallery/DefaultView/DefaultView.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import propTypes from 'proptypes';
 import './DefaultView.css';
+// import ProductThumbnailScroll from '../ProductThumbnailScroll/ProductThumbnailScroll.jsx';
 
-const DefaultView = ({ onClickImage, defaultPhoto }) => {
+const DefaultView = ({ onClickRightChange, onClickChangeThumbnail, style, currentPhoto, onClickImage, defaultPhoto }) => {
   if (defaultPhoto !== undefined) {
     return (
       <div className="default-image">
+        {/* <div className="product-thumbnail"><ProductThumbnailScroll onClickRightChange={onClickRightChange} onClickChangeThumbnail={onClickChangeThumbnail} key={style.style_id} currentPhoto={style.photos[currentPhoto].thumbnail_url} arrayOfPhoto={style} />
+        </div> */}
           <img className="display-image" onClick={onClickImage} alt="My Main Product Display" src={defaultPhoto.url} />
       </div>
     );
