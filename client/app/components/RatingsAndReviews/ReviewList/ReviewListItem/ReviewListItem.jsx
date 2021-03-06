8b17115b1ba5c87c967143b855449fa52dad05df
Karim Zamrini <karim.e.zamrini@gmail.com>
2021-03-06 12:22:04 -0700

    review body show more works and unrenders on click

--- client/app/components/RatingsAndReviews/ReviewList/ReviewListItem/ReviewListItem.jsx
+++ client/app/components/RatingsAndReviews/ReviewList/ReviewListItem/ReviewListItem.jsx
@@ -4,11 +4,39 @@ import StarRating from '../../../StarRating/StarRating.jsx';
 import './ReviewListItem.css';
 
 const ReviewListItem = ({ review }) => {
+  const [reviewTileBody, setReviewTileBody] = useState(review.body.substring(0, 250));
+  const [reviewBodyButtonClass, setReviewBodyButtonClass] = useState('review-show-more-button');
+
   const monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   const month = review.date.slice(5, 7);
   const day = review.date.slice(8, 10);
   const year = review.date.slice(0, 4);
   const formattedString = `${monthArray[month - 1]} ${day}, ${year}`;
+
+  const handleShowMoreBody = () => {
+    setReviewTileBody(review.body);
+    setReviewBodyButtonClass('review-show-more-button-hidden');
+  };
+
+  const ReviewBodyRender = () => {
+    if (review.body.length > 250) {
+      return (
+        <>
+          <div className="reviewListItemBody">
+            {reviewTileBody}
+          </div>
+          <div className="review-show-body-link">
+            <button type="button" className={reviewBodyButtonClass} onClick={handleShowMoreBody}>show more</button>
+          </div>
+        </>
+      );
+    }
+    return (
+      <div className="reviewListItemBody">
+        {reviewTileBody}
+      </div>
+    );
+  };
 
   return (
     <div className="reviewListItem">
@@ -23,9 +51,7 @@ const ReviewListItem = ({ review }) => {
       <div className="reviewListItemSummary">
         {review.summary}
       </div>
-      <div className="reviewListItemBody">
-        {review.body}
-      </div>
+      <ReviewBodyRender />
     </div>
   );
 };
