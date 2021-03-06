94a2d08daa03125c43375b9c8b10fb6c3fd42354
Karim Zamrini <karim.e.zamrini@gmail.com>
2021-03-06 11:03:36 -0700

    added formatted review date to review tile

--- client/app/components/RatingsAndReviews/ReviewList/ReviewListItem/ReviewListItem.jsx
+++ client/app/components/RatingsAndReviews/ReviewList/ReviewListItem/ReviewListItem.jsx
@@ -2,18 +2,30 @@ import React, { useEffect, useState } from 'react';
 import StarRating from '../../../StarRating/StarRating.jsx';
 import './ReviewListItem.css';
 
-const ReviewListItem = ({ review }) => (
-  <div className="reviewListItem">
-    <div className="reviewListItemStarRating">
-      <StarRating reviewScore={review.rating} setMargin="0 0 0 0" />
+const ReviewListItem = ({ review }) => {
+  const monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const month = review.date.slice(5, 7);
+  const day = review.date.slice(8, 10);
+  const year = review.date.slice(0, 4);
+  const formattedString = `${monthArray[month - 1]} ${day}, ${year}`;
+
+  return (
+    <div className="reviewListItem">
+      <div className="reviewListItemStarRating">
+        <StarRating reviewScore={review.rating} setMargin="0 0 0 0" />
+        <div className="review-date">
+          {formattedString}
+        </div>
+      </div>
+      <div className="reviewListItemSummary">
+        {review.summary}
+      </div>
+      <div className="reviewListItemBody">
+        {review.body}
+      </div>
     </div>
-    <div className="reviewListItemSummary">
-      {review.summary}
-    </div>
-    <div className="reviewListItemBody">
-      {review.body}
-    </div>
-  </div>
-);
+  );
+};
+
 
 export default ReviewListItem;
