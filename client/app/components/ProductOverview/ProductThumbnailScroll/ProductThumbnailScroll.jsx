703f63625f8c4bbaf2ce5454bd1267250c91fd5d
Khoi Dao <khoidao55@gmail.com>
2021-03-06 15:16:40 -0800

    added no picture avail if there's no photos

--- client/app/components/ProductOverview/ProductThumbnailScroll/ProductThumbnailScroll.jsx
+++ client/app/components/ProductOverview/ProductThumbnailScroll/ProductThumbnailScroll.jsx
@@ -4,6 +4,12 @@ import './ProductThumbnailScroll.css';
 
 const ProductThumbnailScroll = ({ onClickChangeThumbnail, currentPhoto, arrayOfPhoto }) => {
   if (arrayOfPhoto !== undefined && currentPhoto !== undefined) {
+    if (arrayOfPhoto.photos.length === 1 && currentPhoto === null) {
+      return (
+        <div />
+      );
+    }
+
     return (
       <div className="test">
         {arrayOfPhoto.photos.map((photo) => {
@@ -16,28 +22,13 @@ const ProductThumbnailScroll = ({ onClickChangeThumbnail, currentPhoto, arrayOfP
         <button>Down</button>
       </div>
     );
-
-    // return (
-    //   <div className="test">
-    //     {arrayOfPhoto.photos.map((photo) => {
-    //       return (
-    //       <div key={photo.thumbnail_url} className="thumbnail-photos">
-    //         <img onClick={() => onClickChangeThumbnail(arrayOfPhoto.photos.indexOf(photo))} className={`${currentPhoto === photo.thumbnail_url ? 'selected-image thumbnail-single-photo thumbnail-size' :`thumbnail-single-photo thumbnail-size`}`} src={photo.thumbnail_url} />
-    //       </div>
-    //       )
-    //     })}
-    //   </div>
-    // );
   }
-
-  return (
-    <div />
-  );
 };
 
 ProductThumbnailScroll.propTypes = {
-  currentPhoto: propTypes.string.isRequired,
-  arrayOfPhoto: propTypes.object.isRequired,
+  onClickChangeThumbnail: propTypes.func,
+  currentPhoto: propTypes.string,
+  arrayOfPhoto: propTypes.object,
 };
 
 export default ProductThumbnailScroll;
