2f77773c6ccc4a7dce81c4c4f4bc8b07e5d02b7b
Khoi Dao <khoidao55@gmail.com>
2021-03-05 19:13:54 -0800

    styles now only display 4 circles per line

--- client/app/components/ProductOverview/ProductStyleSelector/ProductStyleSelector.jsx
+++ client/app/components/ProductOverview/ProductStyleSelector/ProductStyleSelector.jsx
@@ -10,7 +10,7 @@ const ProductStyleSelector = ({ handleSelectedStyleClick, styles ,styleName}) =>
           return(
             <span key={productStyle.style_id}>
             {/* <i className="far fa-check-circle"></i> */}
-            <img className="style-image" onClick={() => handleSelectedStyleClick(productStyle, productStyle.original_price)} style={{paddingLeft: '5px', borderRadius: '50%', width: '50px', height: '50px'}} src={productStyle.photos[0].thumbnail_url} />
+            <img className="style-image" onClick={() => handleSelectedStyleClick(productStyle, productStyle.original_price)} style={{marginLeft: '15px', borderRadius: '50%', width: '70px', height: '70px'}} src={productStyle.photos[0].thumbnail_url} />
             </span>
           )
         })}
