976277b9e7cb6387de6f7de8aae2267a6fb6b8bc
tdagai <trew3535@gmail.com>
2021-03-08 10:05:55 -0800

    Added and passed CarouselCard testing

---
+++ client/app/components/Carousels/CarouselCard/CarouselCard.test.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import StarRating from '../../StarRating/StarRating.jsx';
+import CarouselCard from './CarouselCard.jsx';
+
+const dummyCard = {
+  id: 1234,
+  productImage: 'https://bsaber.com/wp-content/uploads/2020/01/a2040f737cc603cf561c54624bde21715b7cbf52.jpg',
+  category: 'meme',
+  name: 'pretty cool bananas',
+  originalPrice: '1234567',
+  salePrice: '123',
+  stars: '3.5',
+  handleActionButton: () => { console.log('Action!'); },
+  handleRedirect: () => { console.log('Redirect!'); },
+  carouselType: 'related',
+  features: [{ feature: 'bananas', value: 'pretty cool' }],
+};
+
+describe('CarouselCard', () => {
+  test('it renders correctly', () => {
+    const wrapper = shallow(<CarouselCard { ...dummyCard } />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
