7c28c95b0e3cba463e28867d7312d6a7aa971647
Karim Zamrini <karim.e.zamrini@gmail.com>
2021-03-08 18:29:18 -0700

    breakdown bars css

--- client/app/components/RatingsAndReviews/RatingSummary/RatingBreakdown/RatingBreakdown.jsx
+++ client/app/components/RatingsAndReviews/RatingSummary/RatingBreakdown/RatingBreakdown.jsx
@@ -4,11 +4,10 @@ import './RatingBreakdown.css';
 const RatingBreakdown = ({ metaData }) => {
   let numberOfReviews = 0;
   let ratingValueArray = Object.values(metaData.ratings);
-  console.log('ratingValueArray: ', ratingValueArray);
+
   ratingValueArray.forEach((value) => {
     numberOfReviews += (value * 1);
   });
-  console.log('number of reviews after "forEach": ', numberOfReviews);
 
   const fiveStarValue = Math.round((metaData.ratings["5"] / numberOfReviews) * 100) || 0;
   const fourStarValue = Math.round((metaData.ratings["4"] / numberOfReviews) * 100) || 0;
@@ -20,27 +19,32 @@ const RatingBreakdown = ({ metaData }) => {
     <div className="breakdown-bar-container">
       <div className="rating-brakdown-bar">
         <label htmlFor="breakdown-progress-bar">5 stars</label>
-        <progress id="breakdown-progress-bar" value={fiveStarValue} max="100">pogChamp</progress>
+        {' '}
+        <progress id="breakdown-progress-bar" className="breakdown-progress-bar" value={fiveStarValue} max="100">pogChamp</progress>
       </div>
 
       <div className="rating-brakdown-bar">
         <label htmlFor="breakdown-progress-bar">4 stars</label>
-        <progress id="breakdown-progress-bar" value={fourStarValue} max="100">pogChamp</progress>
+        {' '}
+        <progress id="breakdown-progress-bar" className="breakdown-progress-bar" value={fourStarValue} max="100">pogChamp</progress>
       </div>
 
       <div className="rating-brakdown-bar">
         <label htmlFor="breakdown-progress-bar">3 stars</label>
-        <progress id="breakdown-progress-bar" value={threeStarValue} max="100">pogChamp</progress>
+        {' '}
+        <progress id="breakdown-progress-bar" className="breakdown-progress-bar" value={threeStarValue} max="100">pogChamp</progress>
       </div>
 
       <div className="rating-brakdown-bar">
         <label htmlFor="breakdown-progress-bar">2 stars</label>
-        <progress id="breakdown-progress-bar" value={twoStarValue} max="100">pogChamp</progress>
+        {' '}
+        <progress id="breakdown-progress-bar" className="breakdown-progress-bar" value={twoStarValue} max="100">pogChamp</progress>
       </div>
 
       <div className="rating-brakdown-bar">
-        <label htmlFor="breakdown-progress-bar">1 star</label>
-        <progress id="breakdown-progress-bar" value={oneStarValue} max="100">pogChamp</progress>
+        <label htmlFor="breakdown-progress-bar">1 stars</label>
+        {' '}
+        <progress id="breakdown-progress-bar" className="breakdown-progress-bar" value={oneStarValue} max="100">pogChamp</progress>
       </div>
     </div>
   );
