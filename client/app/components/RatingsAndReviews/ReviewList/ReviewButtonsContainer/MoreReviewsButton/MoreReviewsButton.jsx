de3af5e2d252a244bd733de1bf49fb046d067ce6
Karim Zamrini <karim.e.zamrini@gmail.com>
2021-03-09 17:57:47 -0700

    module style overhaul

--- client/app/components/RatingsAndReviews/ReviewList/ReviewButtonsContainer/MoreReviewsButton/MoreReviewsButton.jsx
+++ client/app/components/RatingsAndReviews/ReviewList/ReviewButtonsContainer/MoreReviewsButton/MoreReviewsButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const MoreReviewsButton = ({ visibleReviews, setVisibleReviews}) => (
-  <button onClick={() => setVisibleReviews(visibleReviews + 2)} type="button" className="moreReviewsButton">more reviews</button>
+  <button onClick={() => setVisibleReviews(visibleReviews + 2)} type="button" className="moreReviewsButton">More reviews</button>
 );
 
 export default MoreReviewsButton;
