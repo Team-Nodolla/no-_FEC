b33cfa514285ba4afdb9c083c1b3b306d4455c99
tdagai <trew3535@gmail.com>
2021-03-10 15:48:24 -0800

    Added the code for the slider

---
+++ client/app/components/DarkModeSlider/DarkModeSlider.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './DarkModeSlider.css';
+
+const DarkModeSlider = () => (
+  <>
+    <label className="switch">
+      <input type="checkbox" />
+      <span className="slider round" />
+    </label>
+  </>
+);
+
+export default DarkModeSlider;
