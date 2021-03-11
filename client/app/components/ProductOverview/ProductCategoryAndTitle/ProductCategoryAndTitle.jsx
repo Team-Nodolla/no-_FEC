68c196b67859418805a72355bc909c40d1562cab
Khoi Dao <khoidao55@gmail.com>
2021-03-11 09:23:35 -0800

    removed state from catergory and title

--- client/app/components/ProductOverview/ProductCategoryAndTitle/ProductCategoryAndTitle.jsx
+++ client/app/components/ProductOverview/ProductCategoryAndTitle/ProductCategoryAndTitle.jsx
@@ -1,21 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './ProductCategoryAndTitle.css';
 import StarRating from '../../StarRating/StarRating.jsx';
 
 const ProductCategoryAndTitle = ({ reviewNumber, price, product }) => {
   if (product !== undefined && reviewNumber?.results !== undefined) {
-    const [clickedReview, setClickedReview] = useState(false);
-
     const clickedReviewFunc = (e) => {
       e.preventDefault();
       return document.querySelector('#review-list').scrollIntoView();
     };
 
     return (
-      <div className='categoryAndTitleContainer'>
+      <div className="categoryAndTitleContainer">
         <div className={`product-star-${reviewNumber.results.length !== 0 ? 'active' : 'disabled'}`}>
-        <StarRating reviewScore={product.averageRating} setMargin='16px 0 0 0' />
-        <p onClick={(e) => clickedReviewFunc(e)}className='read-all-review'>{`read (${reviewNumber.results.length}) reviews`}</p>
+          <StarRating reviewScore={product.averageRating} setMargin='16px 0 0 0' />
+          <p onClick={(e) => clickedReviewFunc(e)}className='read-all-review'>{`read (${reviewNumber.results.length}) reviews`}</p>
         </div>
         <p>{product.category}</p>
         <h1>{product.name}</h1>
